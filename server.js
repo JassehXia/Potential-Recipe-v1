c3b3a860648c8f4fe94d20d3ab9e80d1df449a95
JassehXia <jassehxia@gmail.com>
2025-09-30 16:04:18 -0400

    Update server to serve frontend and API

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .then(() => console.log("Mongo connected"))
   .catch(err => console.error(err));
 
+const path = require("path");
+
+// Serve static frontend files
+app.use(express.static(path.join(__dirname, "public")));
+
+
+
 app.post("/recipes", async (req, res) => {
   try {
     const recipes = req.body; // expect an array of recipe objects
